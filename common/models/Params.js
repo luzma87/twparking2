360f7e6ed10d64ebfc7054c362e2f6821855fa99
Luz <luzma_87@yahoo.com>
2018-03-31 12:06:17 -0500

    adds Params.doSomething method as an example of insert

--- common/models/Params.js
+++ common/models/Params.js
@@ -1,5 +1,31 @@
 'use strict';
 
-module.exports = function(Params) {
+module.exports = function (Params) {
   Params.disableRemoteMethodByName("deleteById");
+
+  Params.doSomething = function (cb) {
+    console.log("Doing stuff");
+
+    const p = {textoMail: 'Algo ahi', ascendente: true};
+
+    Params.create(p, (err, obj) => {
+      console.log("DONE!");
+      console.log(err);
+      console.log(obj);
+      cb(null, "done");
+    });
+  };
+
+  Params.remoteMethod(
+    'doSomething', {
+      http: {
+        path: '/doSomething', verb: 'get'
+      },
+      returns: {
+        arg: 'status',
+        type: 'string'
+      }
+    }
+  );
+
 };
