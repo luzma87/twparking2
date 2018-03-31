949b9a8323d546717a5d8596338e25b376893377
Luz <luzma_87@yahoo.com>
2018-03-31 10:18:40 -0500

    disables delete methods of relations

--- common/models/Edificio.js
+++ common/models/Edificio.js
@@ -2,4 +2,6 @@
 
 module.exports = function(Edificio) {
   Edificio.disableRemoteMethodByName("deleteById");
+  Edificio.disableRemoteMethodByName('prototype.__destroyById__puestos');
+  Edificio.disableRemoteMethodByName('prototype.__delete__puestos');
 };
--- common/models/Usuario.js
+++ common/models/Usuario.js
@@ -2,4 +2,7 @@
 
 module.exports = function(Usuario) {
   Usuario.disableRemoteMethodByName("deleteById");
+
+  Usuario.disableRemoteMethodByName('prototype.__destroyById__autos');
+  Usuario.disableRemoteMethodByName('prototype.__delete__autos');
 };
