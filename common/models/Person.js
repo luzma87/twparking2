b8351d9eb09c8645f94cb016d5cc90afcc0d0fd5
Luz <luzma_87@yahoo.com>
2018-04-26 07:53:42 -0500

    adds userData to login

--- common/models/Person.js
+++ common/models/Person.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Person) {
   Person.disableRemoteMethodByName('deleteById');
+
+  Person.afterRemote('login', async (ctx, user, next) => {
+    if (user) {
+      let filter = {
+        fields: { password: false, username: false, realm: false },
+        where: { id: user.userId },
+      };
+      let userData = await Person.find(filter);
+      user.userData = userData[0];
+    }
+  });
+
 };
